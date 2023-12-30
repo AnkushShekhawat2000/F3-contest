9bb6e716a78ebd255118c8381a539114def5ef29
Ankush Shekhawat <ankush75056@gmail.com>
2023-12-30 21:46:46 +0530

    final project

--- script.js
+++ script.js
@@ -1,110 +1,82 @@
-// all elements
-const day_heading = document.querySelector("#day_heading");
-const img = document.querySelector("#img_conatiner img");
-const img_heading = document.querySelector("#img_heading");
-const img_info = document.querySelector("#img_info");
-const form = document.querySelector("#search-form");
-const dateInput = document.querySelector("#search-input");
-const search_list = document.querySelector("#search-history");
-
-let prevSearchArray = [];
-
-let currentDate;
-
-const api_key = "CgNagta9ivozVVEsebeezCcfIN9Y3Isj2hzsE6Fb";
-
+const apiKey = "HciKq2pclfFOfUPa9bMX7V6WoDztZb0u4t2sR3uW";
+const searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+const currentDate = new Date().toISOString().slice(0, 10);
 function getCurrentImageOfTheDay() {
-  let today = new Date();
+	const url = `https://api.nasa.gov/planetary/apod?date=${currentDate}&api_key=${apiKey}`;
 
-  //format date in yyyy-mm-dd format
-  const year = today.getFullYear();
-  const month = String(today.getMonth() + 1).padStart(2, 0);
-  const day = String(today.getDate()).padStart(2, 0);
-  const formattedDate = `${year}-${month}-${day}`;
-  currentDate = formattedDate;
-  // print data on UI
-  fetchData(currentDate);
+	fetch(url)
+		.then(response => response.json())
+		.then(data => {
+			
+			const currentImageContainer=document.getElementById('current-image-container');
+            currentImageContainer.innerHTML=`
+            <h1>NASA PICTURE OF THE ${data.date}</h1>
+            <img src="${data.url}" alt="${data.title}">
+            <h3>${data.title}</h3>
+            <p>${data.explanation}</p>
+            `
+		})
+		.catch(error => console.error(error));
 }
 
-async function fetchData(date) {
-  const url = `https://api.nasa.gov/planetary/apod?date=${date}&api_key=${api_key}`;
-  try {
-    const response = await fetch(url);
+function getImageOfTheDay(date) {
+	const url = `https://api.nasa.gov/planetary/apod?date=${date}&api_key=${apiKey}`;
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    // if data is received call printData Function to print the data
-    changeDateHeading(date);
-    printData(data);
-  } catch (error) {
-    console.error("Error fetching data from NASA.com:", error);
-  }
-}
-
-function printData(dataObj) {
-  const explanation = dataObj.explanation;
-  const title = dataObj.title;
-  const img_src = dataObj.url;
-  //cache-busting parameter to the image URL => imestamp
-  const cacheBuster = new Date().getTime();
-  const imgSrcWithCacheBuster = `${img_src}?${cacheBuster}`;
-  img_info.innerHTML = `${explanation}`;
-  img_heading.innerHTML = `${title}`;
-  img.src = `${imgSrcWithCacheBuster}`;
+	fetch(url)
+		.then(response => response.json())
+		.then(data => {
+			const currentImageContainer=document.getElementById('current-image-container');
+            currentImageContainer.innerHTML=`
+            <h1>NASA PICTURE OF THE DAY</h1>
+            <img src="${data.url}" alt="${data.title}">
+            <h3>${data.title}</h3>
+            <p>${data.explanation}</p>
+            `
+			saveSearch(date);
+			addSearchToHistory();
+		})
+		.catch(error => console.error(error));
 }
 
 function saveSearch(date) {
-  prevSearchArray.push({ date: date });
-  localStorage.setItem("searches", JSON.stringify(prevSearchArray));
-  addSearchToHistory();
+	searchHistory.push(date);
+	localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
 }
 
 function addSearchToHistory() {
-  const data = localStorage.getItem("searches");
-  const dateArray = JSON.parse(data);
-  search_list.innerHTML = "";
-  dateArray.forEach((element) => {
-    const listElement = document.createElement("li");
-    const listAnchor = document.createElement("a");
-    listAnchor.innerText = element.date;
-    listAnchor.href = "#";
-    listElement.appendChild(listAnchor);
-    search_list.appendChild(listElement);
-  });
+	const searchHistoryList = document.getElementById('search-history');
+	searchHistoryList.innerHTML = '';
+	searchHistory.forEach(search => {
+		
+        const li = document.createElement('li');
+		const a=document.createElement('a');
+        a.textContent=search;
+        a.setAttribute('href',`? date=${search}`);
+        li.appendChild(a);
+        
+		searchHistoryList.appendChild(li);
+	});
+    
+
+
+	searchHistoryList.addEventListener('click', event => {
+		if (event.target.tagName === 'A') {
+			const date = event.target.textContent;
+			window.location.href=`?date=${date}`;
+            getImageOfTheDay(date);
+			document.getElementById('current-image-container').innerHTML = '';
+			document.getElementById('search-input').value = date;
+		}
+	});
 }
 
-function changeDateHeading(date) {
-  if (date == currentDate) {
-    day_heading.innerHTML = `NASA Picture of the Day`;
-    console.log("golu");
-  } else {
-    day_heading.innerHTML = `Picture on ${date}`;
-  }
-}
 
-//handling the events
-// handling prevSeacrh click event
-search_list.addEventListener("click", (event) => {
-  const date = event.target.innerText;
-  fetchData(date);
-  day_heading.innerHTML = "";
-  day_heading.innerHTML = `Picture on ${date}`;
+getCurrentImageOfTheDay();
+
+const searchForm = document.getElementById('search-form');
+searchForm.addEventListener('submit', event => {
+	event.preventDefault();
+	const date = document.getElementById('search-input').value;
+	getImageOfTheDay(date);
+	document.getElementById('current-image-container').innerHTML = '';
 });
-
-//handling form date click evevnt
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const date = dateInput.value;
-  fetchData(date);
-  day_heading.innerHTML = "";
-
-  //TODO
-  saveSearch(date);
-});
-
-// onload evevnt
-
-window.addEventListener("load", getCurrentImageOfTheDay);
